29bb3249c177fedaac38d003ed0bce18dbd6cbd8
AndrewForresterGit <152125083+AndrewForresterGit@users.noreply.github.com>
2024-04-11 12:52:45 -0400

    Create main.py et imports
    Importation des librairies qu'on va utiliser. Il aura peut-être scipy à ajouter selon nos besoins.

---
+++ main.py
@@ -0,0 +1,5 @@
+"""docstring tres pretinant"""
+import numpy as np
+import matplotlib.pyplot as plt
+# on va utiliser plotly pour le heatmap afin d'illustrer la matrice P
+# import plotly.express as px 

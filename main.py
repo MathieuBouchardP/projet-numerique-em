f07aae28a8965331094474ee6909ee44f4c7393b
Andrew Forrester <andrew.forrester.1@ulaval.ca>
2024-05-02 11:58:34 -0400

    supprimer string inutile

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 """docstring tres pretinant"""
 import numpy as np
 import matplotlib.pyplot as plt
-# on va utiliser plotly pour le heatmap afin d'illustrer la matrice P
-# import plotly.express as px 
+
 
 
